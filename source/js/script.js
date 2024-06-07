ace714c1c972b1f257ab0a219ac36ed9230e6409
Amilton-Melo <amiltonmelo.dev@gmail.com>
2024-06-07 00:11:14 -0300

    Simple Calculator

---
+++ source/js/script.js
@@ -0,0 +1,28 @@
+const number1 = document.getElementById("number1");
+const operation = document.getElementById("operations");
+const number2 = document.getElementById("number2");
+const result = document.getElementById("result-number");
+const btn = document.getElementById("button-calc");
+
+btn.addEventListener("click", () =>{
+    if(operation.value === "+"){
+        console.log("soma");
+        result.value = Number(number1.value) + Number(number2.value);
+    }
+    else if(operation.value === "-"){
+        console.log("diminuir");
+        result.value = Number(number1.value) - Number(number2.value);
+    }
+    else if(operation.value === "*"){
+        console.log("multiplicação");
+        result.value = Number(number1.value) * Number(number2.value);
+    }
+    else if(operation.value === "/"){
+        console.log("divisão");
+        result.value = Number(number1.value) / Number(number2.value);
+    }
+    else if(operation.value === "%"){
+        console.log("resto");
+        result.value = Number(number1.value) % Number(number2.value);
+    }
+})
